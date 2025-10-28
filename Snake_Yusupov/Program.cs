fa3a576c1d0bdaef2ae5b266d85d9868ff15ed9d
student-a502 <student-a502@STUD-RM19-A502>
2025-10-28 17:01:25 +0500

    Созданный метод Send Юсупов

--- Snake_Yusupov/Program.cs
+++ Snake_Yusupov/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using Common;
+using System.Text;
+using System;
+using Newtonsoft.Json;
 
 namespace Snake_Yusupov
 {
@@ -6,11 +12,38 @@ namespace Snake_Yusupov
     {
         public static List<Leaders> Leaders = new List<Leaders>();
         public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
-        public static List<ViewModelGame> viewModelGames = new List<ViewModelGame>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         public static int LocalPort = 5001;
         public static int Speed = 15;
         static void Main(string[] args)
         {
+        }
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                IPAddress.Parse(User.IpAddress),
+                int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(
+                        viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Отправил данные пользователю: (User.IpAddress}:(User.Port}");
+                }
+                catch (Exception exp )
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + exp.Message );
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }
