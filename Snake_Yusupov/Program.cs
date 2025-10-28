2810579c65dad31d0d64d31d5e6e48dd98115019
student-a502 <student-a502@STUD-RM19-A502>
2025-10-28 16:22:02 +0500

    Добавленные переменные сервера Юсупов

--- Snake_Yusupov/Program.cs
+++ Snake_Yusupov/Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Snake_Yusupov
 {
-    internal class Program
+    public class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGame> viewModelGames = new List<ViewModelGame>();
+        public static int LocalPort = 5001;
+        public static int Speed = 15;
         static void Main(string[] args)
         {
         }
