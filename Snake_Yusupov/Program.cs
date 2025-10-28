f65c9d4adda3911fa7eae4a2ee53ec2bcdf77271
student-a502 <student-a502@STUD-RM19-A502>
2025-10-28 17:09:58 +0500

    REceiver недоделаный

--- Snake_Yusupov/Program.cs
+++ Snake_Yusupov/Program.cs
@@ -32,7 +32,7 @@ namespace Snake_Yusupov
                         viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
                     sender.Send(bytes, bytes.Length, endPoint);
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Отправил данные пользователю: (User.IpAddress}:(User.Port}");
+                    Console.WriteLine($"Отправил данные пользователю: {User.IpAddress}:{User.Port}");
                 }
                 catch (Exception exp )
                 {
@@ -45,5 +45,39 @@ namespace Snake_Yusupov
                 }
             }
         }
+        public static void Receiver()
+        {
+            UdpClient receivingUdpClient = new UdpClient();
+            IPEndPoint endPoint = null;
+            try
+            {
+                Console.WriteLine("Команды сервера: ");
+                while (true)
+                {
+                    byte[] receiverData = receivingUdpClient.Receive(ref endPoint);
+                    string returnData = Encoding.UTF8.GetString(receiverData);
+                    if (returnData.ToString().Contains("/start"))
+                    {
+                        string[] dataMessage = returnData.Split('|');
+                        ViewModelUserSettings User = JsonConvert.DeserializeObject<ViewModelUserSettings>(dataMessage[1]);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Подключился пользоиатель: {User.IpAddress}:{User.Port}");
+                        remoteIPAddress.Add(User);
+                        User.IdSnake = AddSnake();
+                        viewModelGames[User.IdSnake].IdSnake = User.IdSnake;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch(Exception exp)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникло исключение: " + exp.Message);
+            }
+        }
+
     }
 }
